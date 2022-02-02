a5a47657d00ff43aece5c3db595d695fb038c668
eliasal <elias.alo@ri.se>
2022-02-02 10:19:17 +0100

    My project first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from math import add_numbers
+from person import Person
+
+
+def alex_method(name):
+    p = Person(name)
+    print(p)
+
+
+if __name__ == '__main__':
+    # TODO:Fix Bug
+    # TODO: Implement Tests
+    print("Pycharm is Awesome")
+    print("Ciao")
+    print("Ciao")
+    name = "Ahmed"
+    print(name)
+    print(add_numbers(2, 2))
+    alex_method("Ali")
+
